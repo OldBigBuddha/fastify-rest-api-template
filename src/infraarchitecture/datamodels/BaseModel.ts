c8f21e2fea19d45e56a9593ca1b7704552fe9797
OldBigBuddha <development@oldbigbuddha.net>
2022-01-19 11:09:58 +0900

    UserModel の再設計

--- src/infraarchitecture/datamodels/BaseModel.ts
+++
@@ -1,9 +0,0 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
-
-export default abstract class BaseModel {
-  @PrimaryGeneratedColumn("increment", { comment: "ID" })
-  id!: number;
-
-  @Column({ type: "uuid", nullable: false, unique: true, comment: "UUID" })
-  uuid!: string;
-}
--- src/infraarchitecture/datamodels/UserModel.ts
+++ src/infraarchitecture/datamodels/UserModel.ts
@@ -1,25 +1,30 @@
-import { Column, Entity } from "typeorm";
-import BaseModel from "./BaseModel";
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
-export enum Role {
-  ADMINISTRATOR = "administrator",
-  USER = "user",
-}
+const TABLE_NAME = "users";
 
-@Entity("users")
-export default class UserModel extends BaseModel {
-  @Column({ type: "char", length: 16, nullable: false, comment: "16文字のランダム文字列; 認証トークン用" })
-  public rnd!: string;
+@Entity(TABLE_NAME)
+export default class UserModel {
+  @PrimaryGeneratedColumn("increment", { type: "bigint", unsigned: true, comment: "内部ID" })
+  readonly id!: number;
 
-  @Column({ type: "enum", enum: Role, default: Role.USER, comment: "役職" })
-  public role!: Role;
+  @Index(`IDX:${TABLE_NAME}:uuid`, {
+    unique: true,
+  })
+  @Column({ type: "uuid", nullable: false })
+  public uuid!: string;
 
   @Column({ type: "varchar", length: 255, nullable: false, comment: "名前" })
-  public name!: string;
+  public displayName!: string;
 
+  @Index(`IDX:${TABLE_NAME}:login_id`, {
+    unique: true,
+  })
   @Column({ type: "varchar", length: 255, unique: true, nullable: false, comment: "ログインID" })
   public loginId!: string;
 
   @Column({ type: "char", length: 254, nullable: false, comment: "ログインパスワードのハッシュ値" })
   public passwordHash!: string;
+
+  @Column({ type: "char", length: 16, nullable: false, comment: "16文字のランダム文字列; 認証トークン用" })
+  public rnd!: string;
 }
