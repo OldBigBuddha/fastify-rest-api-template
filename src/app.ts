4d6b333b981f8e6384b6735eb1a4af5ae2019484
OldBigBuddha <development@oldbigbuddha.net>
2022-03-06 15:28:16 +0900

    cors 対策を導入

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import path from "path";
 
 import Fastify, { FastifyInstance } from "fastify";
+import cors from "fastify-cors";
 import { bootstrap } from "fastify-decorators";
 import Helmet from "fastify-helmet";
 
@@ -17,6 +18,13 @@ function createApp(): FastifyInstance {
   });
 
   app.register(Helmet);
+  app.register(cors, {
+    origin: "*", // FIXME: 適切な値を考える
+    allowedHeaders: ["Authorization", "Content-Type", "Origin", "X-Requested-With"],
+    exposedHeaders: ["Link", "Location"],
+    maxAge: 86400,
+    credentials: true,
+  });
   app.register(bootstrap, {
     directory: path.resolve(__dirname, "controllers"),
     mask: /\.controller\./,
